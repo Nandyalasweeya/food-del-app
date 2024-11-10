381bc1d6a529ff8b8a8b7619a01a9b506dd4e81c
Nandyalasweeya <221fa18183@gmail.com>
2024-11-10 20:16:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
-import Cart from './pages/Cart/Cart'
 import Home from './pages/Home/Home'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Cart from './pages/Cart/Cart'
 import Footer from './components/Footer/Footer'
-
+import LoginPopup from './components/LoginPopup/LoginPopup'
+import Placeorder from './pages/PlaceOrder/PlaceOrder'
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false)
+  
   return (
     <>
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder/>}/>
-      </Routes>
-    </div>
-    <Footer/>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      <div className='app'>
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          < Route path='/' element={<Home />} />
+          < Route path='/Cart' element={<Cart />} />
+          < Route path='/Order' element={<Placeorder />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
+
   )
 }
-
 export default App
